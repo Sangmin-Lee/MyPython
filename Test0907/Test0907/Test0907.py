a420142e5fa3c952865e4720ae02672e929ae62a
Sangmin-Lee <gov8687@naver.com>
2015-09-07 13:31:10 +0900

    Test0907
    0907실습

---
+++ Test0907/Test0907/Test0907.py
@@ -0,0 +1,47 @@
+#b = [1,2,3]
+#c = ['Life', 'is', 'too', 'short']
+
+#print(b+c)      #strcat
+#f = b+c
+#print(f)
+#f[3] = 'greenjoa'
+#print(f[3:4])   #List
+##[3:4] == (3<= x < 4)
+#print(f[3])     #Variable
+#print(b*3)      #반복
+#f.insert(3,'e') #insert
+#f.append('asdf')#마지막에 추가
+#f.remove('asdf')#직접 쓴 것과 비교해서 삭제
+#f[1:2] = []     #빈칸으로 만들어 삭제
+#print(f)
+#del f[0]    #인자값으로 삭제
+#print(f)
+#print(f.index('greenjoa')) #이 데이터가 없으면 에러 발생
+
+id = ['sangmin', 'boha']    #아이디 옆에 패스워드 추가하기
+id.insert(id.index('sangmin')+1, 'tkdals')
+print(id)
+id.insert(2+1, 'qhgk')
+print(id)
+
+sub = ['dltkdals', '01051305773']   #패스워드 옆에 추가정보 넣기
+id.insert(id.index('tkdals')+1, sub)
+print(id)
+
+nId = len(id)
+print(range(nId))
+for steps in range(nId) :   # 들여쓰기로 for문 구분 맨 끝에는 :(콜론) 
+    print(id[steps])        # range 함수도 list 생성
+for steps in id :
+    print(steps)
+
+scores = [85, 62, 37, 83, 10, 29]
+scores.sort()   #오름차순
+print(scores)
+scores.reverse()#그냥 뒤집기
+print(scores)
+
+top5 = []   #정렬해서 큰 순서대로 5개 출력
+for steps in range(5) :
+    top5.insert(steps, scores[steps])
+print(top5)
